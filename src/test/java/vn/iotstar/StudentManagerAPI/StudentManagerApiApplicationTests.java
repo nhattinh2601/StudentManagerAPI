547063a41bf150fd1fc067396f8b91b93221b94f
nhattinh2601 <nhattinh2601@gmail.com>
2023-08-11 09:30:13 +0700

    up

---
+++ src/test/java/vn/iotstar/StudentManagerAPI/StudentManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.iotstar.StudentManagerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
